25a9403d1f51b3df4ebb8d14105026d9ca34bdba
André Almeida <andre.almeida@mare.arditi.pt>
2024-10-28 12:11:14 +0000

    dropdown edge click fix

--- src/views/Dashboard/Common/RowOperation.jsx
+++ src/views/Dashboard/Common/RowOperation.jsx
@@ -3,9 +3,25 @@ import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 
+const StyledDropdown = styled.div`
+  & > ul.ant-dropdown-menu > li.ant-dropdown-menu-item {
+    padding: 0;
+  }
+
+  &
+    > ul.ant-dropdown-menu
+    > li.ant-dropdown-menu-item
+    > .ant-dropdown-menu-title-content
+    span {
+    padding: 5px 12px;
+    width: 100%;
+  }
+`;
+
 const StyledDropdownLink = styled.span`
   color: inherit;
 `;
+
 const RowOperation = (props) => {
   const { onDeleteConfirm, onUpdateClick, permissions = [] } = props;
   const [items, setItems] = useState([]);
@@ -17,8 +33,8 @@ const RowOperation = (props) => {
       itemsAux.push({
         key: "1",
         label: onUpdateClick && (
-          <a onClick={() => onUpdateClick()} href="#">
-            Update
+          <a onClick={() => onUpdateClick()}>
+            <span>Update</span>
           </a>
         ),
       });
@@ -47,7 +63,18 @@ const RowOperation = (props) => {
   }
 
   return (
-    <Dropdown menu={{ items }}>
+    <Dropdown
+      dropdownRender={(menu) => (
+        <StyledDropdown
+          style={{
+            backgroundColor: "white",
+          }}
+        >
+          {menu}
+        </StyledDropdown>
+      )}
+      menu={{ items }}
+    >
       <StyledDropdownLink className="ant-dropdown-link">
         <img src="/assets/icons/dropdown.svg" alt="dropdown" />
       </StyledDropdownLink>
