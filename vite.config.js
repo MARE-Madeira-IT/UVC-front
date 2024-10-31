1a76479e40ff8f1cd650b2f267d7c716bc7211f9
Rúben Freitas <joseruben98@hotmail.com>
2024-10-31 16:57:26 +0000

    vite base change

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         },
     },
     // base: "/",
-    base: "./",
+    base: "https://mare-madeira-it.github.io/UVC-front/",
     preview: {
         port: 3000,
         strictPort: true,
