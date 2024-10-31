dd6bb6b0b3fb8721c736432e353ce28c0776ebfc
Rúben Freitas <joseruben98@hotmail.com>
2024-10-31 16:16:06 +0000

    prod test

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
             redux_modules: "/redux/redux-modules",
         },
     },
-    base: "/",
+    // base: "/",
+    base: "./",
     preview: {
         port: 3000,
         strictPort: true,
